7e478b77089e7085eb09bdd034a47a9637505e24
TheODDYSEY <mosessabz07@gmail.com>
2023-12-31 19:47:40 +0300

    Add ClerkProvider and update dependencies ts: 26:08 mn

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link"
+import Image from "next/image"
 
 const Header = () => {
   return (
-    <div>Header</div>
+   <header className="w-full border-b">
+    <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36 ">
+            <Image src="/assets/images/logo.svg" 
+            alt="Evently Logo" 
+            width={128} 
+            height={38}/>
+        </Link>
+        <div className="flex w-32 justify-end gap-3">
+            
+
+        </div>
+    </div>
+   </header>
   )
 }
 
