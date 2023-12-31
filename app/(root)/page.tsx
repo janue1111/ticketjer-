78ea5ec9e47ae2bbc073e190a6370f804ff3b261
TheODDYSEY <mosessabz07@gmail.com>
2023-12-31 18:50:57 +0300

    Remove unnecessary code

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,6 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h1 className='text-4xl'>Evently</h1>
-      <Button variant="destructive" className="px-10">Hello</Button>
     </main>
   )
 }
