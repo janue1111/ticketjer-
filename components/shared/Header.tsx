531d8268f6104de1ae0286fefbf75c736f1f8d8c
TheODDYSEY <mosessabz07@gmail.com>
2024-01-01 23:56:52 +0300

    Fix formatting in Header component

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
               <Button asChild className='rounded-full' size='lg' >
                 <Link href="/sign-in">
                   Login
-                  </Link>
+                </Link>
               </Button>
             </SignedOut>
         </div>
