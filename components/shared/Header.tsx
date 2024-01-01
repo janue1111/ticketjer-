f3cb9442dbfc8fc5dd7cd232587a3a7b23274ba7
TheODDYSEY <mosessabz07@gmail.com>
2024-01-01 23:55:32 +0300

    Fix formatting in Header component

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -16,7 +16,9 @@ const Header = () => {
         <div className="flex w-32 justify-end gap-3">
             <SignedOut>
               <Button asChild className='rounded-full' size='lg' >
-                <Link href="/sign-in">Login</Link>
+                <Link href="/sign-in">
+                  Login
+                  </Link>
               </Button>
             </SignedOut>
         </div>
