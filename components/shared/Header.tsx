7077aa247b1d6cb8af257a525891cd4e9c088b75
TheODDYSEY <mosessabz07@gmail.com>
2024-01-03 15:03:27 +0300

    Add @radix-ui/react-dialog and update Header component

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -2,6 +2,8 @@ import Link from "next/link"
 import Image from "next/image"
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import { Button } from "../ui/button"
+import NavItems from "./NavItems"
+import MobileNav from "./MobileNav"
 
 const Header = () => {
   return (
@@ -13,9 +15,16 @@ const Header = () => {
             width={128} 
             height={38}/>
         </Link>
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs ">
+            <NavItems/>
+          </nav>
+        </SignedIn>
+        
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
+            <MobileNav/>
           </SignedIn>
             <SignedOut>
               <Button asChild className='rounded-full' size='lg' >
