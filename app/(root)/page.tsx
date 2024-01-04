96bc867b24b30a58b5d59f0e97df3f05076a6dce
TheODDYSEY <mosessabz07@gmail.com>
2024-01-04 23:59:40 +0300

    Continuning with content

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -28,11 +28,14 @@ export default function Home() {
       />
     </div>
   </section>
-  <section
-  id='events'
-  className='wrapper my-8 flex flex-col gap-8 md:gap-12'
-  >
-
+  <section id='events' className='wrapper my-8 flex flex-col gap-8 md:gap-12'>
+    <h2 className='h2-bold'>
+      Thousands of Events
+    </h2>
+    <div className='flex w-full flex-col gap-5 md:flex-row'>
+      Search
+      CategoryFilter
+    </div>
   </section>
   </>
   )
