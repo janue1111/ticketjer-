ca3e707c5d6cc4ec5933448dd8c447cf3c678d4e
TheODDYSEY <mosessabz07@gmail.com>
2024-01-11 10:51:31 +0300

    Start Datepicker

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -21,6 +21,9 @@ import { Textarea } from "@/components/ui/textarea";
 import { FileUploader } from "./FileUploader";
 import { useState } from "react";
 import Image from "next/image";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 
 type EventFormProps = {
   userId: string;
@@ -122,7 +125,7 @@ const EventForm = ({ userId, type }: EventFormProps) => {
             render={({ field }) => (
               <FormItem className="w-full">
                 <FormControl>
-                  <div className="flex-center h-[55px] w-full overflow-hidden rounded-full bg-gray-50 px-4 py-2">
+                  <div className="flex-center h-[55px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
                     <Image
                       src="/assets/icons/location-grey.svg"
                       alt="calendar"
@@ -133,6 +136,33 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                       {...field}
                       className="input-field"
                     />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="startDateTime"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <div className="flex-center h-[55px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+                    <Image
+                      src="/assets/icons/calendar.svg"
+                      alt="calendar"
+                      height={24}
+                      width={24}
+                      className="filter-grey"
+                    />
+                    <p className="ml-3 whitespace-nowrap text-grey-600"> Start Date:</p>
+                    <DatePicker 
+                    selected={field.value} 
+                    onChange={(date:Date) => field.onChange(date)} />
                   </div>
                 </FormControl>
                 <FormMessage />
