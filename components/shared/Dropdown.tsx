d7364e22d1b16697013a76c84c4e58d11209c278
TheODDYSEY <mosessabz07@gmail.com>
2024-01-14 22:22:42 +0300

    Add category creation and retrieval functionality to Dropdown component

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -18,8 +18,9 @@ import {
 } from "@/components/ui/alert-dialog";
 
 import { ICategory } from "@/lib/database/models/category.model";
-import { startTransition, useState } from "react";
+import { startTransition, useEffect, useState } from "react";
 import { Input } from "../ui/input";
+import { createCategory, getAllCategories } from "@/lib/actions/category.actions";
 
 type DropdownProps = {
   value?: string;
@@ -30,8 +31,23 @@ const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
   const [categories, setCategories] = useState<ICategory[]>([]);
   const [newCategory, setNewCategory] = useState('');
   const handleAddCategory =() =>{
-      
+      createCategory({
+        categoryName:newCategory.trim()
+      })
+      .then((category)=>{
+        setCategories((prevState)=> [...prevState,category])
+      })
   }
+
+  useEffect(()=>{
+    const getCategories = async()=>{
+      const categoryList = await getAllCategories();
+      
+      categoryList && setCategories(categoryList as ICategory[])
+    }
+
+    getCategories();
+  },[])
 
 
   return (
