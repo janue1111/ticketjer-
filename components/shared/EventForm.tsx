6afc0d0be76925711cbb10da72762fd879eb190b
TheODDYSEY <mosessabz07@gmail.com>
2024-01-17 09:46:31 +0300

    Add image upload functionality to EventForm

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -23,7 +23,9 @@ import { useState } from "react";
 import Image from "next/image";
 import DatePicker from "react-datepicker";
 import { Checkbox } from "@/components/ui/checkbox";
+import {useUploadThing} from '@lib/uploadthing'
 import "react-datepicker/dist/react-datepicker.css";
+
 
 type EventFormProps = {
   userId: string;
@@ -32,6 +34,8 @@ type EventFormProps = {
 const EventForm = ({ userId, type }: EventFormProps) => {
   const [files, setFiles] = useState<File[]>([]);
   const initialValues = eventDefaultValues;
+
+  const {startUpLoad} = useUploadThing('imageUpLoader')
   // 1. Define your form.
   const form = useForm<z.infer<typeof eventFormSchema>>({
     resolver: zodResolver(eventFormSchema),
@@ -40,9 +44,13 @@ const EventForm = ({ userId, type }: EventFormProps) => {
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof eventFormSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+    const eventData = values ;
+
+    let uploadedImageUrl = values.imageUrl;
+
+    if(files.length > 0 ){
+      const uploadedImages = await startUpLoad(files)
+    }
   }
 
   return (
