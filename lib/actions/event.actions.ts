e594c424775547972dc3f82601924ac2c20b5ae4
TheODDYSEY <mosessabz07@gmail.com>
2024-02-19 14:33:22 +0300

    Started Event Details Page

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,11 +1,17 @@
-"use server"
+"use server";
 
 import { CreateEventParams } from "@/types";
 import { handleError } from "../utils";
 import { connectToDatabase } from "../database";
 import User from "../database/models/user.model";
 import Event from "../database/models/event.model";
+import Category from "../database/models/category.model";
 
+const populateEvent = async (query: any) => {
+  return query
+  .populate({path: "organizer", model: User, select: "_id firstName lastName",})
+  .populate({path: "category", model: Category, select: "_id name",})
+}
 export const createEvent = async ({
   event,
   userId,
@@ -23,17 +29,32 @@ export const createEvent = async ({
     console.log({
       categoryId: event.categoryId,
       organizerId: userId,
-    })
+    });
 
     const newEvent = await Event.create({
       ...event,
       category: event.categoryId,
-      organizer: userId
+      organizer: userId,
     });
 
     return JSON.parse(JSON.stringify(newEvent));
   } catch (error) {
     console.log(error);
+    handleError(error);
+  }
+};
+
+export const getEventById = async (eventId: string) => {
+  try {
+    await connectToDatabase();
+
+    const event = await Event.findById(eventId);
+    if (!event) {
+      throw new Error("Event not found");
+    }
+
+    return JSON.parse(JSON.stringify(event));
+  } catch (error) {
     handleError(error);
   }
 };
