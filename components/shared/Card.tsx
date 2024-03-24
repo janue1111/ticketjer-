63daaca8f184731eb73cfe065973534404b4d6f0
TheODDYSEY <mosessabz07@gmail.com>
2024-03-24 22:46:13 +0300

    Added price feature

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -11,26 +11,29 @@ type CardProps = {
 const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
   return (
     <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
-      <Link 
+      <Link
         href={`/events/${event._id}`}
-        style={{backgroundImage: `url(${event.imageUrl})`}}
+        style={{ backgroundImage: `url(${event.imageUrl})` }}
         className="flex-center flex-grow bg-gray-50 bg-cover bg-center text-grey-500"
       />
-        {/* IS EVENT CREATOR */}
-        <Link 
+      {/* IS EVENT CREATOR */}
+      <Link
         href={`/events/${event._id}`}
         className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4"
+      >
+        <div
+          className="flex gap-2"
         >
-            <div
-            className="flex gap-2"
-            >
-                <span>
-                    {event.isFree ? 'FREE' : `$${event.price}`}
-                </span>
+          <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
+            {event.isFree ? 'FREE' : `$${event.price}`}
+          </span>
+          <p className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500">
+            {event.category.name}
+          </p>
 
-            </div>
-        </Link>
-      
+        </div>
+      </Link>
+
     </div>
   );
 };
