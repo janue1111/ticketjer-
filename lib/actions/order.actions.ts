76cc2597047e72ddda48e882becb66ef57c7916f
TheODDYSEY <mosessabz07@gmail.com>
2024-10-12 21:41:40 +0300

    ts 4:45:21 adding stripe webhook

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -3,6 +3,9 @@
 import Stripe from "stripe"
 import { CheckoutOrderParams } from "@/types";
 import { redirect } from "next/navigation";
+import { connect } from "http2";
+import { connectToDatabase } from "../database";
+import Order from "../database/models/order.model";
 
 export const checkoutOrder = async (order: CheckoutOrderParams) => {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
@@ -37,3 +40,20 @@ export const checkoutOrder = async (order: CheckoutOrderParams) => {
     throw error;
   }
 };
+
+export const createOrder = async (order: CheckoutOrderParams) => {
+  try {
+
+    await connectToDatabase();
+    const newOrder = await Order.create({
+      ...order,
+      event:order.eventId,
+      buyer: order.buyerId,
+    })
+    return JSON.parse(JSON.stringify(newOrder));
+
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
