030ce3bfcba1e68357f0ec89564aa11ad28ad98b
TheODDYSEY <mosessabz07@gmail.com>
2024-10-22 07:12:38 +0300

    Fin search component ts 5:26:08

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,12 +4,18 @@ import Image from 'next/image'
 import Collection from '@/components/shared/Collection'
 import { getAllEvents } from '@/lib/actions/event.actions';
 import Search from '@/components/shared/Search';
+import { SearchParamProps } from '@/types';
+import CategoryFilter from '@/components/shared/CategoryFilter';
 
-export default async function Home() {
+export default async function Home({searchParams}: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = searchParams?.query as string || '';
+  const category = searchParams?.category as string || '';
+
   const events= await getAllEvents({
-    query:'',
-    category:'',
-    page:1,
+    query:searchText,
+    category,
+    page,
     limit:6
   })
 
@@ -45,7 +51,7 @@ export default async function Home() {
     </h2>
     <div className='flex w-full flex-col gap-5 md:flex-row'>
       <Search/>
-      CategoryFilter
+      <CategoryFilter/>
     </div>
     <Collection
     data={events?.data}
