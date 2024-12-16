6605948fc950071dd286edc73298311522ca6c4b
TheODDYSEY <mosessabz07@gmail.com>
2024-12-16 14:49:36 +0300

    Revamped homepage content and layout for improved user engagement

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -22,47 +22,49 @@ export default async function Home({searchParams}: SearchParamProps) {
   // console.log(events)
   return (
   <>
-  <section className='bg-primary-50 bg-dotted-pattern bg-container py-5 md:py-10'>
-    <div className='wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0'>
-      <div className='flex flex-col justify-center gap-8'>
-      <h1 className='h1-bold'>Host, Connect, Celebrate: Your Events, Our Platform!</h1>
-      <p className='p-regular-20 md:p-regular-24'>Book and learn helpful tips from 3,168+ mentors in world-class companies with our global</p>
+<section className='bg-primary-50 bg-dotted-pattern bg-container py-5 md:py-10'>
+  <div className='wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0'>
+    <div className='flex flex-col justify-center gap-8'>
+      <h1 className='h1-bold'>
+        Discover, Connect, and Celebrate with TicketiHub
+      </h1>
+      <p className='p-regular-20 md:p-regular-24'>
+        Join a community of event enthusiasts and gain access to 3,016+ curated events and opportunities to connect with world-class organizers.
+      </p>
       <Button size="lg" asChild className="button w-full sm:w-fit">
-        <Link
-        href="#events"
-        >
-          Explore Now
+        <Link href="#events">
+          Browse Events
         </Link>
       </Button>
-
-      </div>
-      <Image
+    </div>
+    <Image
       src="/assets/images/hero.png"
-      alt="hero"
+      alt="Discover exciting events"
       width={1000}
       height={1000}
       className='max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]'
-      />
-    </div>
-  </section>
-  <section id='events' className='wrapper my-8 flex flex-col gap-8 md:gap-12'>
-    <h2 className='h2-bold'>
-      Thousands of Events
-    </h2>
-    <div className='flex w-full flex-col gap-5 md:flex-row'>
-      <Search/>
-      <CategoryFilter/>
-    </div>
-    <Collection
+    />
+  </div>
+</section>
+<section id='events' className='wrapper my-8 flex flex-col gap-8 md:gap-12'>
+  <h2 className='h2-bold'>
+    Explore Thousands of Unique Experiences
+  </h2>
+  <div className='flex w-full flex-col gap-5 md:flex-row'>
+    <Search/>
+    <CategoryFilter/>
+  </div>
+  <Collection
     data={events?.data}
-    emptyTitle="No events found"
-    emptyStateSubtext="Come back soon for more events!"
+    emptyTitle="No Events Available"
+    emptyStateSubtext="Check back soon for more amazing events!"
     collectionType="All_Events"
     limit={6}
     page={page}
     totalPages={events?.totalPages}
-    />
-  </section>
+  />
+</section>
+
   </>
   )
 }
