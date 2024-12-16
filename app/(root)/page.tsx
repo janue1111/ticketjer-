bc74a2cb1f4a080dfaa4297b69645207d89665b8
TheODDYSEY <mosessabz07@gmail.com>
2024-12-16 17:06:23 +0300

    Edit content 📄

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -47,8 +47,8 @@ export default async function Home({searchParams}: SearchParamProps) {
   </div>
 </section>
 <section id='events' className='wrapper my-8 flex flex-col gap-8 md:gap-12'>
-  <h2 className='h2-bold'>
-    Explore Thousands of Unique Experiences
+  <h2 className='h3-bold'>
+    Thousands of Events
   </h2>
   <div className='flex w-full flex-col gap-5 md:flex-row'>
     <Search/>
