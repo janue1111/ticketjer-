b563ec7696d67f8edc14f003fb92b809b7362840
janue1111 <206374324+janue1111@users.noreply.github.com>
2025-08-05 12:40:54 -0500

    style: Change background to contain for event cards

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -23,7 +23,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
       <Link
         href={`/events/${event._id}`}
         style={{ backgroundImage: `url(${event.imageUrl})` }}
-        className="flex-center flex-grow bg-gray-50 bg-cover bg-center text-grey-500"
+        className="flex-center flex-grow bg-gray-50 bg-contain bg-center bg-no-repeat text-grey-500"
       />
       {/* IS EVENT CREATOR */}
       {isEventCreator && !hidePrice && (
