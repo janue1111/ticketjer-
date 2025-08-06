0002bcf84fe4c308b07bd71b2fbfa763b5237d51
janue1111 <206374324+janue1111@users.noreply.github.com>
2025-08-05 22:07:16 -0500

    feat: Add environment-specific Stripe webhook secrets

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -6,7 +6,9 @@ export async function POST(request: Request) {
   const body = await request.text()
 
   const sig = request.headers.get('stripe-signature') as string
-  const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!
+  const endpointSecret = process.env.NODE_ENV === 'production' 
+  ? process.env.STRIPE_WEBHOOK_SECRET_PROD!
+  : process.env.STRIPE_WEBHOOK_SECRET_DEV!
 
   let event
 
