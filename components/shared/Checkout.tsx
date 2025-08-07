f5586321327ea9373872a4918e796bcd5d7cb8ec
janue1111 <206374324+janue1111@users.noreply.github.com>
2025-08-07 09:56:57 -0500

    solucion de cantidad de compras y conexión de esta con Stripe

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -1,43 +1,70 @@
-import React, { useEffect } from "react";
+"use client"
+
+import React, { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { IEvent } from "@/lib/database/models/event.model";
 
 import { loadStripe } from '@stripe/stripe-js';
 import { checkoutOrder } from "@/lib/actions/order.actions";
+import { Input } from "../ui/input";
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
 loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
-const Checkout = ({event,userId}:{event :IEvent,userId:string} ) => {
+const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
+  const [quantity, setQuantity] = useState(1);
 
   useEffect(() => {
     // Check to see if this is a redirect back from Checkout
     const query = new URLSearchParams(window.location.search);
-    if (query.get('success')) {
-      console.log('Order placed! You will receive an email confirmation.');
+    if (query.get("success")) {
+      console.log("Order placed! You will receive an email confirmation.");
     }
 
-    if (query.get('canceled')) {
-      console.log('Order canceled -- continue to shop around and checkout when you’re ready.');
+    if (query.get("canceled")) {
+      console.log(
+        "Order canceled -- continue to shop around and checkout when you’re ready."
+      );
     }
   }, []);
+
   const onCheckout = async () => {
-    const order ={
+    const order = {
       eventTitle: event.title,
       eventId: event._id,
-      price:event.price,
-      isFree : event.isFree,
-      buyerId: userId
-    }
+      price: event.price,
+      isFree: event.isFree,
+      buyerId: userId,
+      quantity: quantity,
+    };
 
-    await checkoutOrder (order);
+    await checkoutOrder(order);
   };
-  return <form action={onCheckout} method="post">
-    <Button type="submit" role="link" size="lg" className="button sm:w-fit">
-      {event.isFree ?'Get Ticket':'Buy Ticket'}
-    </Button>
-  </form>;
+
+  const handleQuantityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(e.target.value, 10);
+    if (value > 0) {
+      setQuantity(value);
+    }
+  };
+
+  return (
+    <form action={onCheckout} method="post">
+      <div className="flex items-center gap-3">
+        <p className="whitespace-nowrap">Cantidad:</p>
+        <Input
+          type="number"
+          value={quantity}
+          onChange={handleQuantityChange}
+          className="w-16 text-center rounded-md"
+        />
+        <Button type="submit" role="link" size="lg" className="button sm:w-fit">
+          {event.isFree ? "Get Ticket" : "Buy Ticket"}
+        </Button>
+      </div>
+    </form>
+  );
 };
 
 export default Checkout;
