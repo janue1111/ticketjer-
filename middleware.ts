ed5135078021ea3ecc8e2a2beafe95ad0f8b1f7f
janue1111 <206374324+janue1111@users.noreply.github.com>
2025-12-22 22:25:35 -0500

    fix: add public routes to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher([
+  '/',
+  '/events(.*)',
   '/api/webhook/clerk',
   '/api/webhook/izipay',
   '/api/uploadthing',
